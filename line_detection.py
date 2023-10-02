12457d4706fe0dcbd0577d7160ca148ea69ca867
Ikuo Obataya <i.obataya@gmail.com>
2023-10-03 08:16:32 +0900

    Switching to use TopoStats

---
+++ line_detection.py
@@ -0,0 +1,219 @@
+"""Functions for reading and writing data."""
+import os
+import logging
+from datetime import datetime
+import io
+import struct
+from pathlib import Path
+import re
+
+import numpy as np
+from ruamel.yaml import YAML, YAMLError
+from ruamel.yaml.main import round_trip_load as yaml_load, round_trip_dump as yaml_dump
+import yaml
+
+from topostats.logs.logs import LOGGER_NAME
+
+LOGGER = logging.getLogger(LOGGER_NAME)
+
+class LinearMolecule:
+    """
+    Numpy配列 [y座標配列, x座標配列, シグナル値配列]をインスタンスとして持つ。-> yx_values
+    ラベルされたImage、xy配列、から生成する。メソッド間のやり取りには、内部の処理はこのyx_values配列を使う。
+    MatPlotLibでimshowするときだけ展開する。
+
+    ※座標配列 - 入出力、描画
+    [y][x] : yx_pos, shape(N,2)
+    [y,x]:   yx_pos.T, shape(2,N)
+
+    ※ベクター配列 - 距離計算, 象限判別
+    [[yx_pos_idx],[yx_pos_idx],[yx_pos]], shape(N,N,2)
+    """
+    def __init__(self,yx_values, mol_idx=1):
+        self.mol_idx = mol_idx
+        self.orig_yx_values = yx_values
+        self.orig_left = self.orig_x().min()
+        self.orig_top = self.orig_y().min()
+        self.width = int(self.orig_x().max() - self.orig_left + 1)
+        self.height = int(self.orig_y().max() - self.orig_top + 1)
+        self.x = self.orig_x() - self.orig_left
+        self.y = self.orig_y() - self.orig_top
+        self.orig_yxT = np.array([self.orig_y(),self.orig_x()]).T
+        self.yxT = np.array([self.y,self.x]).T
+
+    def orig_x(self):
+        return self.orig_yx_values[1]
+
+    def orig_y(self):
+        return self.orig_yx_values[0]
+
+    def values(self):
+        return self.orig_yx_values[2]
+
+    def get_yx(self, pix_idx):
+        return (self.y[pix_idx],self.x[pix_idx])
+
+    def get_orig_yx(self,pix_idx):
+        return (self.orig_y()[pix_idx], self.orig_x()[pix_idx])
+
+    def get_vector(self, pix_idx0, pix_idx1):
+        return (self.yxT[pix_idx0], self.yxT[pix_idx1])
+
+    def get_displacement(self, pix_idx0, pix_idx1):
+        return self.yxT[pix_idx0] - self.yxT[pix_idx1]
+
+    def get_orig_vector(self,pix_idx0, pix_idx1):
+        return (self.orig_yxT[pix_idx0], self.orig_yxT[pix_idx1])
+
+    def count(self):
+        return len(self.x)
+
+    @staticmethod
+    def create_from_labelled_image(labelled_image, mol_idx=1):
+        l = np.where(labelled_image==mol_idx)
+        # prepare nparray of y, x
+        (y_ar, x_ar) = (l[0], l[1])
+        values = np.zeros((len(x_ar),))
+        for i in range(0,len(y_ar)):
+            (y, x) = (y_ar[i], x_ar[i])
+            values[i] = labelled_image[y][x]
+        return Molecule(np.array([y_ar,x_ar,values]),mol_idx=mol_idx)
+
+    @staticmethod
+    def create_from_yx_array(y_array,x_array,value=1.0,mol_idx=1):
+        val_array = np.full(len(y_array),value)
+        mol = Molecule(np.array([y_array,x_array,val_array],dtype=[np.int32,np.int32,np.float32]),mol_idx=mol_idx)
+        return mol
+
+    def get_displacement_matrix(self):
+        # [y座標配列, x座標配列]から転置した[y,x]配列、yxTを使う。
+        # [y,x]をaxis=2としたマトリックスにする。
+        m1 = self.yxT.reshape((1,self.count(),2))
+        m2 = self.yxT.reshape((self.count(),1,2))
+        mv = m1 - m2  # 2点間の全ベクトル
+        return mv
+
+    def filter_by_quadrant(self):
+        vectors = self.get_displacement_matrix()
+        result = np.where(vectors < 0) # 負の値の要素をすべて探す。-> 返り値は[[pix_idx],[pix_idx],[idxY or idxX],]
+        negative_y = np.where(result[2]==0) # idxYがゼロのときその要素はY座標。-> 返り値は[[idxY],]
+        for pix_idx in negative_y[0]:
+            idx0 = result[0][pix_idx]
+            idx1 = result[1][pix_idx]
+            vectors[idx0][idx1] = [0,0]
+        return vectors
+
+    def filter_by_length(self, min_length=0, filter_quad=True):
+        if filter_quad:
+            vectors = self.filter_by_quadrant()
+        else:
+            vectors = self.get_displacement_matrix()
+        msq = np.sum(np.square(vectors),axis=2) # y,xを二乗してsum -> 長さ2乗
+        md = np.sqrt(ms) # 平方根を取って距離に。
+        length_filtered_mask = np.where(md>=min_length, 1, 0)  # 最小の長さを超えていれば距離, if not 0
+        pix_pairs = np.where(length_filtered_mask > 0)
+        return pix_pairs,length_filtered_mask
+
+    def get_image(self):
+        """ get binary image of this molecule"""
+        image = self.get_blank_image()
+        for i in range(0,len(self.x)):
+            y = int(self.y[i])
+            x = int(self.x[i])
+            image[y][x] = 1
+        return image
+
+    def get_blank_image(self):
+        return np.zeros((self.height,self.width))
+
+    @staticmethod
+    def plot_image(image, aspect = 1.0):
+        fig, ax = plt.subplots(figsize=(4, 4))
+        plt.imshow(image, cmap="afmhot", aspect=aspect)
+        plt.show()
+
+    def __str__(self):
+        return f"ID:{self.mol_idx}:{self.count()} pixels, (x,y,w,h)=({self.orig_left},{self.orig_top},{self.width},{self.height})"
+
+    def __repr__(self):
+        return f"ID:{self.mol_idx}({self.count()}),({self.orig_left},{self.orig_top},{self.width},{self.height})"
+
+    def get_line_mask(self,pix_id1,pix_id2):
+        yx1 = self.get_yx(pix_id1)
+        yx2 = self.get_yx(pix_id2)
+        (x1, y1) = (int(yx1[1]), int(yx1[0]))
+        (x2, y2) = (int(yx2[1]), int(yx2[0]))
+        mask = self.get_blank_image()
+        self.draw_line(x1,y1,x2,y2,mask)
+        return mask
+
+    def draw_line(self,x0,y0,x1,y1,np_area):
+        """ Returns ndarray drawin the line by 1
+        Args:
+            (x0,y0,x1,y1) (int): starting and end point of line
+            np_area (np.nd_array): nd_array to draw the line as 1
+        Returns:
+            (np.nd_array): area drawn
+        """
+        def _set_pixel(x,y,np_area,value=1):
+            np_area[y][x] = 1
+
+        def _horizontal_line(x0,y0,x1,y1,np_area):
+            dx = abs(x1 - x0)
+            step = 1
+            if x0 > x1:
+                step = -1
+            (x, y) = (x0, y0)
+            for i in range(dx):
+                _set_pixel(x,y,np_area)
+                x += step
+
+            _set_pixel(x1,y1,np_area)
+            return np_area
+
+        def _vertical_line(x0,y0,x1,y1,np_area):
+            dy = abs(y1 - y0)
+            step = 1
+            if y0 > y1:
+                step = -1
+            (x, y) = (x0, y0)
+            for i in range(dy):
+                _set_pixel(x,y,np_area)
+                y += step
+            _set_pixel(x1,y1,np_area)
+            return np_area
+
+        def _bresenham(x0,y0,x1,y1,np_area):
+            """
+            based on
+            https://github.com/encukou/bresenham/blob/master/bresenham.py
+            """
+            (dx, dy) = (x1 - x0, y1 - y0)
+            xsign = 1 if dx > 0 else -1
+            ysign = 1 if dy > 0 else -1
+            (dx, dy) = (abs(dx),abs(dy))
+            if dx > dy:
+                (xx, xy, yx, yy) = (xsign, 0, 0, ysign)
+            else:
+                (dx, dy) = (dy, dx)
+                (xx, xy, yx, yy) = (0, ysign, xsign, 0)
+            D = 2*dy - dx
+            y = 0
+            for x in range(dx + 1):
+                _set_pixel(x0 + x*xx + y*yx, y0 + x*xy + y*yy, np_area)
+                if D >= 0:
+                    y += 1
+                    D -= 2*dx
+                D += 2*dy
+            return np_area
+
+        if x0 == x1 and y0 == y1:
+            return np_area
+        if x0 == x1 :  # vertical
+            ret_ndarray = _vertical_line(x0,y0,x1,y1,np_area)
+        elif y0 == y1:  # horizontal
+            ret_ndarray = _horizontal_line(x0,y0,x1,y1,np_area)
+        else:
+            ret_ndarray = _bresenham(x0,y0,x1,y1,np_area)
+
+        return ret_ndarray
--- test/test_line_detection.py
+++ test/test_line_detection.py
@@ -1,161 +1,31 @@
-import math, importlib, os, sys, time, unittest, random
+"""Tests of IO"""
+from pathlib import Path
+from unittest import TestCase
+from datetime import datetime
+import numpy as np
+import pytest
 
-PATH_TO_TEST = os.getenv("LINE_DET")
-sys.path.append(PATH_TO_TEST)
-from vectors import V, VV, VecValue, VMap, VMapList, VMapIO
-from line_detection import LineDetection, GridTiler, VFilter
+from topostats.line_detection import (
+    LinearMolecule,
+)
 
+BASE_DIR = Path.cwd()
+RESOURCES = BASE_DIR / "tests" / "resources"
 
-def get_random_VMap(count, min_v=(0,0), max_v=(10,10), val_minmax=(0.0, 1.0)):
-    """ Generate random VMap
-    """
-    ret_vmap = VMap()
-    (min_x, min_y) = min_v
-    (max_x, max_y) = max_v
-    (val_min, val_max) = val_minmax
-    val_range = val_max - val_min
-    while(len(ret_vmap)<count):
-        x = random.randint(min_x, max_x)
-        y = random.randint(min_y, max_y)
-        val = val_min + random.randint(0,100) * val_range / 100
-        ret_vmap[V(x,y)] = val
-    return ret_vmap
-
-class TestLineDetection(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestLineDetection, self).__init__(*args, **kwargs)
-
-    def test_constructor(self):
-        vmap = get_random_VMap(10)
-        ld = LineDetection(vmap,min_length=0, max_length = 200,allowed_empty=2,enable_cache=True)
-        self.assertEqual(ld.vv_count, 10 * 9 / 2)
-
-    def test_sum_along_line(self):
-        test_map = VMap()
-        test_map.add_line(VV(V(10, 10),V(20, 20)), value = 1.0)
-        test_map.set_value(V(10,10), 2.0)
-        test_map.set_value(V(10,20), 3.0)
-        ld = LineDetection(test_map, allowed_empty = 3)
-        sum_diagonal = ld.sum_along_line(test_map, VV(V(10,10),V(20,20)))
-        sum_horizontal = ld.sum_along_line(test_map, VV(V(10,10),V(20,10)))
-        sum_vertical = ld.sum_along_line(test_map, VV(V(10,10),V(10,20)))
-        sum_none = ld.sum_along_line(test_map, VV(V(15,10),V(20,15)))
-
-        self.assertEqual(sum_diagonal, 12) # continuous 2.0 at V(10,10) and 1 x 10
-        self.assertEqual(sum_horizontal, 0) # 2.0 at V(10,10)
-        self.assertEqual(sum_vertical, 0) # 2.0 at V(10,10), many empty by V(10,20)
-        self.assertEqual(sum_none, 0)
+def test_create_from_labelled_image() -> None:
+    labelled_ar = get_labelled_img_1()
+    mol = LinearMolecule.create_from_labelled_image(labelled_ar,mol_idx=2)
+    assert mol.width == 1
+    assert mol.height == 4
 
 
-    def test_drawings(self):
-        PRINT = True
-        vmap = VMap()
-        line = VV(V(10,10),V(40,20))
-        pixels = line.get_pixels_along()
-        pixels.sort()
-        vmap.add_line(line,  value = 1.0)
-        self.assertTrue(V(10,10) in vmap)
-        self.assertTrue(V(25,15) in vmap)
-        self.assertTrue(V(40,20) in vmap)
-
-        line_rev = line.swap()
-        vmap = VMap()
-        vmap.add_line(line_rev, value = 1.0)
-        vmap = vmap.round()
-        self.assertTrue(V(10,10) in vmap)
-        self.assertTrue(V(25,15) in vmap)
-        self.assertTrue(V(40,20) in vmap)
-
-        vmap = VMap()
-        vmap.add_rect(VV(V(10,10),V(20,20)))
-        vmap = vmap.round()
-        self.assertTrue(V(15,10) in vmap)
-        self.assertTrue(V(20,15) in vmap)
-        self.assertTrue(V(15,20) in vmap)
-        self.assertTrue(V(10,15) in vmap)
-
-    def test_str(self):
-        vmap = VMap(list_vecs = [V(1,1),V(2,2)], fill_value = 0.5)
-        str = vmap.str_list()
-
-class TestVFilter(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestVFilter, self).__init__(*args, **kwargs)
-
-    def test_by_count(self):
-        # VMap test, only vmap3 should be excluded.
-        vmap3 = get_random_VMap(3)
-        vmap10 = get_random_VMap(10)
-        vmap20 = get_random_VMap(20)
-        filter_cnt = VFilter([vmap10,vmap20,vmap3],5,max_value=20)
-        self.assertTrue(filter_cnt.by_count())
-        self.assertTrue(filter_cnt.by_count())
-        self.assertTrue(filter_cnt.by_count())
-        self.assertFalse(filter_cnt.by_count())
-        self.assertEqual(len(filter_cnt.filtered), 2)
-
-        # VVMap test, large_vvmap should be excluded.
-        vmapLeftTop = get_random_VMap(3,min_v=(0,0),max_v=(3,3))
-        long_vmap = get_random_VMap(3,min_v=(50,50),max_v=(55,55))
-        long_vmap.add_vmap(vmapLeftTop)
-        small_vmap = get_random_VMap(10,min_v=(20,20),max_v=(30,30))
-        #print(long_vvmap)
-        #print(small_vvmap)
-#        filter_disp = VFilter([long_vvmap,small_vvmap],30,max_value=100)
-###        self.assertTrue(filter_disp.by_count())
-   #     self.assertTrue(filter_disp.by_count())
-#        self.assertFalse(filter_disp.by_count())
-#        self.assertEqual(len(filter_disp.filtered), 1)
+def get_labelled_img_1() -> np.ndarray:
+    labelled_img = np.array([
+        0,0,0,2,1,
+        0,0,1,2,0,
+        3,0,0,2,0,
+        0,0,0,2,0])
+    return labelled_img
 
 
-class TestGridTiler(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestGridTiler, self).__init__(*args, **kwargs)
 
-    def test_constructor(self):
-        # SORT_BY_COUNT test
-        vmaps = []
-        for i in range(0,10):
-            (x, y) = (random.randint(0,100), random.randint(0,100))
-            vmap = VMap()
-            vmap.add_line(VV(V(x, y), V(x+i, y+i)))
-            vmaps.append(vmap)
-        tiler_cnt = GridTiler(vmaps)
-        tiler_cnt.sort_by_count(reverse=True)
-        self.assertEqual(len(tiler_cnt.to_be_tiled[0]), 10)
-        # SORT_BY_ANGLE test
-        vmap1 = VMap(list_vecs = [V(0,10),V(20,0)])
-        vmap2 = VMap(list_vecs = [V(-10,-20),V(0,0),V(-5,-5)])
-        tiler_angle = GridTiler([vmap1,vmap2])
-        tiler_angle.sort_by_angle()
-        self.assertEqual(len(tiler_angle.to_be_tiled[0]),2)
-        tiler_angle = GridTiler([vmap2,vmap1])
-        tiler_angle.sort_by_angle(reverse=True)
-        self.assertEqual(len(tiler_angle.to_be_tiled[0]),3)
-        # SORT_BY_MAX_VALUE test
-        vmap1 = VMap(list_vecvalues=[VecValue(V(1,2),3),VecValue(V(2,2),5)])
-        vmap2 = VMap(list_vecvalues=[VecValue(V(5,6),10),VecValue(V(7,8),20)])
-        tiler_max = GridTiler([vmap1,vmap2])
-        tiler_max.sort_by_max_value(reverse=True)
-        vmap_with_max_value = tiler_max.to_be_tiled[0]
-        (min_val,max_val) = vmap_with_max_value.get_min_max_values()
-        self.assertEqual(max_val.value, 20)
-
-    def test_tiling_add(self):
-        vmap1 = VMap(list_vecs = [V(0,11),V(13,0)])
-        vmap2 = VMap(list_vecs = [V(-10,0),V(-10,-10),V(-5,-5)])
-        vmap3 = VMap(list_vecs = [V(2,3),V(3,4)])
-        tiler_angle = GridTiler([vmap1,vmap2,vmap3])
-        tiler_angle.sort_by_angle()
-        #print(tiler_angle)
-        self.assertEqual(tiler_angle.grid_w, 14)
-        self.assertEqual(tiler_angle.grid_h, 12)
-        self.assertEqual(tiler_angle.tiling_idx, 0)
-        tiler_angle.tiling_add()
-        self.assertEqual(tiler_angle.tiling_idx, 1)
-        next_xy = tiler_angle._tiling_xy()
-        self.assertEqual(next_xy.x(), 14)
-        #print(tiler_angle)
-
-if __name__=='__main__':
-    unittest.main()
